2b60ae6fffca53319bfd200377127401394f286c
Peter Polak <peter.polak.mail@gmail.com>
2022-11-10 18:27:01 +0100

    Refactor script

--- git_commit_push.bat
+++ git_commit_push.bat
@@ -1,5 +1,4 @@
-D:
-cd D:\Programming\C#\Led\Led.BlazorServerWebApp\bin\Release\net6.0\publish\
+cd bin\Release\net7.0\publish\
 git add .
 git commit -a -m "Update production build"
 git push origin master
