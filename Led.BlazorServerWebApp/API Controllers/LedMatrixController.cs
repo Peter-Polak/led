ce56276239b7a281e60d1d4b2ebfbb2c8118f561
Peter Polak <peter.polak.mail@gmail.com>
2022-11-14 11:31:53 +0100

    Refactor code

--- Led.BlazorServerWebApp/API Controllers/LedMatrixController.cs
+++ Led.BlazorServerWebApp/API Controllers/LedMatrixController.cs
@@ -9,7 +9,7 @@ using SixLabors.ImageSharp.PixelFormats;
 
 namespace Led.BlazorServerWebApp.API_Controllers
 {
-    [Route("api")]
+    [Route("api/matrix")]
     [ApiController]
     public class LedMatrixController : ControllerBase
     {
@@ -19,42 +19,19 @@ namespace Led.BlazorServerWebApp.API_Controllers
             this.matrix = matrix;
         }
 
-        // GET: api/<LedMatrixController>
-        [HttpGet]
-        public IEnumerable<string> Get()
+        // PUT api/matrix/power
+        [HttpPut("power")]
+        public void SetMatrixPower(bool isOn)
         {
-            return new string[] { "value1", "value2" };
+            matrix.IsOn = isOn;
         }
 
-        // GET api/<LedMatrixController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        // PUT api/matrix/image/bike
+        [HttpPut("image/bike")]
+        public void RenderBike()
         {
-            return "value";
-        }
-
-        // POST api/bike
-        [HttpPost("bike")]
-        public void Post()
-        {
-            matrix.IsOn = true;
             matrix.CancelCurrentTask(false);
-            matrix.Clear();
-
-            var imageFile = Image.Load<Rgb24>(Media.Image.Bike.GetRelativePathWeb());
-            matrix.DrawImage(imageFile, Media.Image.Bike.GetRelativePathWeb());
-        }
-
-        // PUT api/<LedMatrixController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<LedMatrixController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            matrix.DrawImage(Media.Image.Bike);
         }
     }
 }
